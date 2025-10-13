d8f73784f1db29bee5969a1d8c660dec045865d8
Gustave Curtil <gustave.curtil@tutanota.com>
2025-10-14 00:50:54 +0200

    allez dan

--- draaien.js
+++ draaien.js
@@ -34,11 +34,11 @@ function updatePage() {
     });
 }
 
-// let lastTouchEnd = 0;
-// document.addEventListener('touchend', function (event) {
-//   const now = new Date().getTime();
-//   if (now - lastTouchEnd <= 300) {
-//     event.preventDefault(); // prevent double-tap zoom
-//   }
-//   lastTouchEnd = now;
-// }, false);
+  let lastTouchEnd = 0;
+  document.addEventListener('touchend', (event) => {
+    const now = Date.now();
+    if (now - lastTouchEnd <= 300) {
+      event.preventDefault();
+    }
+    lastTouchEnd = now;
+  }, false);
